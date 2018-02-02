afdc86dee2a830c68dd93528c375b255f2f733ff
Shirley Zhang <shirleyz9402@yahoo.com>
2018-02-02 23:57:23 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,7 +9,7 @@ def nyc_pigeon_organizer(pigeon_data)
        if pigeon_list[name].keys.include?(attribute) == false
          pigeon_list[name][attribute] = []
        end 
-       if pigeon_data[attribute][description][names].include?(name) == true
+       if names.include?(name) == true
          pigeon_list[name][attribute] << description
        end 
      end 
