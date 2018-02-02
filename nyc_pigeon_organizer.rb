78a833d970a9af09a44f065998f84d45bb1ae359
Shirley Zhang <shirleyz9402@yahoo.com>
2018-02-02 23:18:53 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,8 @@
 def nyc_pigeon_organizer(data)
  pigeon_list = {}
- 
+ pigeon_data.each do |attribute, descriptions|
+   descriptions.each do |description, name|
+     pigeon_list = {
+       
+     }
 end
