cc041357136e13b8b80010a664df24f70f128828
Shirley Zhang <shirleyz9402@yahoo.com>
2018-02-03 00:00:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+ pigeon_list = {}
+ pigeon_data.each do |attribute, descriptions|
+   descriptions.each do |description, names|
+     names.each do |name|
+       if pigeon_list.keys.include?(name) == false
+         pigeon_list[name] = {}
+       end 
+       if pigeon_list[name].keys.include?(attribute) == false
+         pigeon_list[name][attribute] = []
+       end 
+       if names.include?(name) == true
+         pigeon_list[name][attribute] << description.to_s
+       end 
+     end 
+   end 
+ end
+ pigeon_list
 end
